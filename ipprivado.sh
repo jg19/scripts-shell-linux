a529faa85215c3264b119601332232ca5bcb1c67
Gustavo Soares de Lima <ghtp25@gmail.com>
2014-12-29 07:57:32 -0200

    Create ipprivado.sh

---
+++ ipprivado.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Autor: Gustavo S. de Lima
+# Email: ghtp25@gmail.com
+# Descrição: captura ip privado e mostra ao usuário
+
+# Função de captura de IP privado
+EnderecoIP ()
+{
+	IP+=($( compgen -W "$( PATH="$PATH:/sbin" LC_ALL=C ifconfig -a | sed -ne 's/.*addr:\([^[:space:]]*\).*/\1/p' \
+	-ne 's/.*inet[[:space:]]\{1,\}\([^[:space:]]*\).*/\1/p' )" -- "$cur" ))
+}
+
+# Chamando a função propriamente dita
+EnderecoIP
+
+# Imprimindo o resultado da função na tela
+echo "$USER, seu IP privado neste momento é: $IP"
+
+# FIM
