0f9d48707a0082cfa360c8ac3b5e3bfc55ad85a8
Gustavo Soares de Lima <ghtp25@gmail.com>
2014-12-30 13:41:23 -0200

    Update megasena.sh

--- megasena.sh
+++ megasena.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# Lista números que já caíram e quantas vezes
+# Lista número que já caíram na megasena de forma ordenada
 
-wget http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip && unzip D_megase.zip && cat D_MEGA.HTM | grep -E -a -A 6 '[0-9]{2}/[0-9]{2}/[0-9]{4}' | sed 's/<td>// ; s/<\/td>// ; s/--//' | grep -E '[0-9]{2}' | grep -vE '[0-9]{2}\/[0-9]{2}\/[0-9]{4}' | sed 's/.*>//' | sort | uniq -c | sort
+wget http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip
+unzip D_megase.zip && cat D_MEGA.HTM | grep -E -a -A 6 '[0-9]{2}/[0-9]{2}/[0-9]{4}' \
+| sed 's/<td>// ; s/<\/td>// ; s/--//' \
+| grep -E '[0-9]{2}' | grep -vE '[0-9]{2}\/[0-9]{2}\/[0-9]{4}' \
+| sed 's/.*>//' | sort | uniq -c | sort
