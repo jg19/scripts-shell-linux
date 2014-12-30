e2bd726d64eb9b8eeff75035d50486f9b9c36f2e
Gustavo Soares de Lima <ghtp25@gmail.com>
2014-12-30 13:45:42 -0200

    Update megasena.sh

--- megasena.sh
+++ megasena.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-# Lista número que já caíram na megasena de forma ordenada
+# Autor: Gustavo S. de Lima
+# Email: ghtp@gmail.com
+# Descrição: Gera lista dos números que já caíram na megasena de forma ordenada
 
 wget http://www1.caixa.gov.br/loterias/_arquivos/loterias/D_megase.zip
 unzip D_megase.zip && cat D_MEGA.HTM | grep -E -a -A 6 '[0-9]{2}/[0-9]{2}/[0-9]{4}' \
