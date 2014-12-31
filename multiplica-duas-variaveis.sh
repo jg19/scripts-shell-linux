fad218810886be762f550f2b313e469f6172eb4b
Gustavo Soares de Lima <ghtp25@gmail.com>
2014-12-30 23:51:54 -0200

    Create multiplica-duas-variaveis.sh

---
+++ multiplica-duas-variaveis.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# Descrição: multipla dois valores
+# visando auxiliar o entendo das variáveis
+
+echo "Vamos calcular x * y?"
+echo "Diga um valor para x:"
+read x
+echo "Diga um valor para y:"
+read y
+echo "x*y = $x*$y = $[x*y]"
