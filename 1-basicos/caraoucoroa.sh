33615e81309e012695794972c67150fedfd7a1ec
Gustavo Soares de Lima <ghtp25@gmail.com>
2015-01-03 14:48:36 -0200

    update

--- 1-basicos/caraoucoroa.sh
+++ 1-basicos/caraoucoroa.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 # Descrição: usa variável RANDOM para mostrar cara ou coroa
 
+# Estabelece uma variável chamada "MOEDA"
+# onde seu conteúdo é a outra variável, neste caso $RANDOM
 MOEDA="$((RANDOM %2))"
 if [[ $MOEDA -eq 0 ]]; then
 echo "Resultado: cara"
 else
 echo "Resultado: coroa"
 fi
+
+# Neste script podemos ver as seguintes questões
+# - O uso de uma variável dentro de outra
+# - A variável $RANDOM
+# - O usdo de if, else para tratamento do resultado
--- 1-basicos/confereroot.sh
+++ 1-basicos/confereroot.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 # Descrição: confere se usuário é root ou não
 
+# Testa se a saída da variável $UID é 0 ou não
 if [ $UID != 0 ]; then
 echo "Usuário não confirmado como root."
 else
 echo "Ok, usuário confirmado como root."
 exit 0
 fi
+
+# Neste script podemos ver:
+# - O uso da variável $UID
+# - O uso de if, else no tratamento do resultado
