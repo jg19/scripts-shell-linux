5135d4a8527c9640124b976033052e9dca358aa6
Gustavo Soares de Lima <ghtp25@gmail.com>
2015-01-03 15:47:42 -0200

    update

--- 1-basicos/demovariaveis.sh
+++ 1-basicos/demovariaveis.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 # Descrição: exemplo de variáveis especiais
 
+# A cada echo é informada uma variável especial
 echo "O nome deste script é $0"
 echo "O PID deste script é $$"
 echo "O total de argumentos utilizados foi $#"
 echo "Todos os argumentos: $*"
 echo "O primeiro argumento utilizado foi $1 e o segundo foi $2"
 echo "E o status do comando anterior foi $?"
+
+# Neste script vimos
+# - Uso das variáveis especiais
--- 1-basicos/diferente.sh
+++ 1-basicos/diferente.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 # Descrição: exemplo read e if com "!" para diferente
 
+# Solicita para o usuário digitar um número
 echo "Digite um número:"
+# Guarda o número em uma variável
 read numero
 if [ $numero != 0 ]; then
 echo "Esse número não é zero"
@@ -9,3 +11,6 @@ else
 echo "Esse número é zero"
 fi
 
+# Neste script vimos:
+# - Uso do read para capturar variável
+# - Uso de if, else para tratamento de resultado
