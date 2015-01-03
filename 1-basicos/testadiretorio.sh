728bf691279805bba25d1c960f417e97d8e74a82
Gustavo Soares de Lima <ghtp25@gmail.com>
2015-01-03 20:27:11 -0200

    update

--- 1-basicos/testadiretorio.sh
+++ 1-basicos/testadiretorio.sh
@@ -3,8 +3,8 @@
 
 echo "Digite um caminho para testar se é diretório:"
 read caminho
-if test -d $caminho; then
-echo "$caminho é um diretório"
-else
-echo "$caminho não é um diretório"
-fi
+	if test -d $caminho; then
+	echo "$caminho é um diretório"
+	else
+	echo "$caminho não é um diretório"
+	fi
