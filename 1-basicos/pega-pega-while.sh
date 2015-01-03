9136b88a314d21106a7faaf8eb24f46debe1b13e
Gustavo Soares de Lima <ghtp25@gmail.com>
2015-01-03 19:29:09 -0200

    update

--- 1-basicos/pega-pega-while.sh
+++ 1-basicos/pega-pega-while.sh
@@ -1,11 +1,20 @@
 #!/bin/bash
 # Descrição: exemplo de pega-pega com while
 
+# Fornecendo valor 1 para variável "n"
 n=1
-while test "$n" -le 10
-do
-echo "Número $n"
-n=$[n+1]
-sleep 1
-done
-echo "Pronto, lá vou eu!"
+	# Enquanto a variável "n" for menor ou igual a 10
+	while test "$n" -le 10
+	do
+		# Informando valor da variável
+		echo "Número $n"
+		# Somando +1 na variáveç
+		n=$[n+1]
+		# Dormindo 1 segundo	
+		sleep 1
+	done
+	echo "Pronto, lá vou eu!"
+
+# Neste script podemos ver:
+# - Soma de variável
+# - Uso do while
