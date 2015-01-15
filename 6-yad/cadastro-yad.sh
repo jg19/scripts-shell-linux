186994f6aacac47ca4f11469849529e947af91be
Gustavo Soares de Lima <ghtp25@gmail.com>
2015-01-15 16:30:50 -0200

    Create cadastro-yad.sh

---
+++ 6-yad/cadastro-yad.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Exemplo de cadastro feito com Yad
+# Autor: Gustavo Soares de Lima
+
+yad --title "Formulário de Cadastro feito em Shell" \
+--width=400 \
+--form --field "Nome:" --field "Digite uma senha:":H \
+--field "Informe o número da sorte":NUM \
+--field "Solteiro(a)":CHK \
+--field "Casado(a)":CHK \
+--field "Selecione uma foto":FL \
+--field "Escolha a fonte de preferência":FN \
+--field "Selecione sua data de nascimento":DT \
+--field "Indique a média de kilómetros/hora que você costuma dirigir":SCL \
+--field "Digite aqui uma frase que gosta":TXT
