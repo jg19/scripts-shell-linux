86d5ae5905a9ce8b3b46f1567db51820c971241a
Gustavo Soares de Lima <ghtp25@gmail.com>
2015-01-18 20:53:37 -0200

    Delete readsenha.sh

--- 1-basicos/readsenha.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Descrição: exemplo read escondendo resposta
-
-echo "Digite a senha:"
-read -s senha
-echo "A senha digitada foi: $senha"
-
-# Neste script podemos ver:
-# - Uso do read com opção "-s" para captura silenciosa
