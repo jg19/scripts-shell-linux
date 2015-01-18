ca6ae9da0ffb1f6e681a400ac5100babe332f9d6
Gustavo Soares de Lima <ghtp25@gmail.com>
2015-01-18 20:55:21 -0200

    Delete variaveis2.sh

--- 1-basicos/variaveis2.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Descrição: exemplo de variáveis
-
-echo "Usuário $LOGNAME. Você está usando o display $DISPLAY. \
-Sua pasta pessoal fica em $HOME. Você saberia me dizer o que \
-contém nos seguintes diretórios: $PATH ?"
